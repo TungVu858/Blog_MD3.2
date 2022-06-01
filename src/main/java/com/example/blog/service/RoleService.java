5d7aa0b6de5fc55139534c5e10a559e673d4434c
TungVu858 <tungvt858@gmail.com>
2022-06-01 22:00:17 +0700

    1/6-tung tạo các class cần thiết

---
+++ src/main/java/com/example/blog/service/RoleService.java
@@ -0,0 +1,7 @@
+package com.example.blog.service;
+
+import com.example.blog.model.Role;
+import com.example.blog.service.GeneralService;
+
+public interface RoleService extends GeneralService<Role> {
+}
