eeedf2d079cb9b5c178875edffe0ddf55c4981de
TungVu858 <tungvt858@gmail.com>
2022-06-02 13:41:45 +0700

    2/6-tung push lại

---
+++ src/main/java/com/example/blog/service/impl/UserServiceImpl.java
@@ -0,0 +1,180 @@
+package com.example.blog.service.impl;
+
+import com.example.blog.model.Role;
+import com.example.blog.model.User;
+import com.example.blog.service.UserService;
+
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.sql.DriverManager.getConnection;
+
+public class UserServiceImpl implements UserService {
+    RoleServiceImpl roleService = new RoleServiceImpl();
+    public UserServiceImpl() {
+    }
+
+    protected Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/blog?useSSL=false", "root", "123456");
+        } catch (SQLException e) {
+
+        } catch (ClassNotFoundException e) {
+
+        }
+        return connection;
+    }
+    @Override
+    public void add(User user) throws SQLException {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("insert into user(username,password,name,email,roleId,status ) values (?,?,?,?,?,? )");) {
+            preparedStatement.setString(1, user.getUsername());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setString(3,user.getName());
+            preparedStatement.setString(4,user.getEmail());
+            preparedStatement.setInt(5,user.getRoleId().getId());
+            preparedStatement.setInt(6,user.getStatus());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+
+        }
+
+    }
+
+    @Override
+    public User findById(int id) {
+        User user = null;
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("select * from user where id =?");) {
+            preparedStatement.setInt(1, id);
+            System.out.println(preparedStatement);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                String username = rs.getString("username");
+                String password = rs.getString("password");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                int roleId = rs.getInt("roleId");
+                String roleName = rs.getString("roleName");
+                Role role = new Role(roleId,roleName);
+                int status = rs.getInt("status");
+                user = new User(id, username, password,name,email,role,status);
+            }
+        } catch (SQLException e) {
+        }
+        return user;
+    }
+
+    @Override
+    public List<User> findAll() {
+        List<User> users = new ArrayList<>();
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("select * from user where (roleId=2) and status=1" );) {
+            System.out.println(preparedStatement);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String username = rs.getString("username");
+                String password = rs.getString("password");
+                String name = rs.getString("name");
+                String phone = rs.getString("phone");
+                int roleId = rs.getInt("roleId");
+                String roleName = rs.getString("roleName");
+                Role role = roleService.findById(roleId);
+                int status = rs.getInt("status");
+                users.add(new User(id, username, password,name,phone,role,status));
+            }
+        } catch (SQLException e) {
+
+        }
+        return users;
+    }
+
+
+    @Override
+    public List<User> findByName(String name) {
+        List<User> users = new ArrayList<>();
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("select * from user where (roleId=2) and name like ? and status = 1; ");) {
+            preparedStatement.setString(1, '%'+name+'%');
+            System.out.println(preparedStatement);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String username = rs.getString("username");
+                String password = rs.getString("password");
+                String nameFind = rs.getString("name");
+                String phone = rs.getString("phone");
+                int roleId = rs.getInt("roleId");
+                String roleName = rs.getString("roleName");
+                Role role = new Role(roleId,roleName);
+                int status = rs.getInt("status");
+                users.add(new User(id,username,password,nameFind,phone,role,status));
+            }
+        } catch (SQLException e) {
+
+        }
+        return users;
+    }
+
+    public List<User> findByUserName(String name) {
+        List<User> user = new ArrayList<>();
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("select * from user where username = ?; ");) {
+            preparedStatement.setString(1, name);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String usernameFind = rs.getString("username");
+                String password = rs.getString("password");
+                String nameFind = rs.getString("name");
+                String email = rs.getString("email");
+                int roleId = rs.getInt("roleId");
+//                String roleName = rs.getString("roleName");
+                Role role = roleService.findById(roleId);
+                int status = rs.getInt("status");
+                user.add(new User(id,usernameFind,password,nameFind,email,role,status));
+                return user;
+            }
+        } catch (SQLException e) {
+
+        }
+        return null;
+    }
+    @Override
+    public boolean delete(int id) throws SQLException {
+        boolean rowDeleted;
+        try (Connection connection = getConnection(); PreparedStatement statement = connection.prepareStatement("delete from user where id=?");) {
+            statement.setInt(1, id);
+            rowDeleted = statement.executeUpdate() > 0;
+        }
+        return rowDeleted;
+    }
+    public boolean delete(User user) throws SQLException{
+        boolean rowDeleted;
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("update user set status=2 where id=?;");) {
+            preparedStatement.setInt(1, user.getId());
+            rowDeleted = preparedStatement.executeUpdate() > 0;
+        }
+        return rowDeleted;
+    }
+    @Override
+    public boolean update(User user) throws SQLException {
+        boolean rowUpdated;
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("update user set password=?,name=?,email =?,roleId=? where id=?;");) {
+            preparedStatement.setString(1,user.getPassword());
+            preparedStatement.setString(2, user.getName());
+            preparedStatement.setString(3, user.getEmail());
+            preparedStatement.setInt(4, user.getRoleId().getId());
+            preparedStatement.setInt(5, user.getId());
+            rowUpdated = preparedStatement.executeUpdate() > 0;
+        }
+        return rowUpdated;
+    }
+}
---
+++ src/main/webapp/register/register.jsp
@@ -0,0 +1,47 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registation</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <meta charset="utf-8"/>
+</head>
+<body>
+<div id="wrapper">
+    <div class="container">
+        <div class="row justify-content-around">
+            <form method="post" class="col-md-6 bg-light p-3 my-3">
+                <h1 class="text-center text-uppercase h3">Đăng kí tài khoản</h1>
+                <input type="hidden" name="action" value="create">
+                <input type="hidden" name="id">
+                <div class="form-group">
+                    <label for="username">Tên tài khoản</label>
+                    <input type="text" name="username" id="username" class="form-control" required pattern="^\w+$">
+                </div>
+                <div class="form-group">
+                    <label for="password">Mật khẩu</label>
+                    <input type="password" name="password" id="password" class="form-control" required pattern="^\w+$">
+                </div>
+                <div class="form-group">
+                    <label for="name">Tên hiển thị</label>
+                    <input type="text" name="name" id="name" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="name">Email</label>
+                    <input type="email" name="email" id="email" class="form-control">
+                </div>
+                <input type="hidden" name="roleId" value="2">
+                <input type="hidden" name="status" value="1">
+                <span style="color: red" class="text-center" >${mess}</span>
+                <br>
+                <input type="submit" value="Đăng ký" class="btn-primary btn-block btn">
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
