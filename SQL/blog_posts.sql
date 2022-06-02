cdff44263b70c6d27a5ebfe6513ff47606a4b83d
Your Name <phunglam11d@gamil.com>
2022-06-02 11:27:48 +0700

    commit

--- SQL/blog_posts.sql
+++ SQL/blog_posts.sql
@@ -3,8 +3,8 @@
 -- Host: localhost    Database: blog
 -- ------------------------------------------------------
 -- Server version	8.0.29
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+use blog;
+    /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!50503 SET NAMES utf8 */;
